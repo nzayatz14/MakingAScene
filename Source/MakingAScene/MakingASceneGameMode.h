cec987a1f4a05c843c906acdebaeb908a6569936
CSC101 <csc101@warcraft.local>
2015-09-13 13:28:19 -0400

    initial commit

---
+++ Source/MakingAScene/MakingASceneGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MakingASceneGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAKINGASCENE_API AMakingASceneGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
